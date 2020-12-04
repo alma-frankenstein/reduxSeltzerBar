90b391119c72afadc97a77de878e2ee3c70d9c57
Alma <a.q.frankenstein@gmail.com>
2020-12-04 10:50:50 -0800

    import root reducer to index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 import { createStore } from 'redux';
-import reducer from './reducers/flavor-list-reducer';
+// import reducer from './reducers/flavor-list-reducer';
+import reducer from './reducers/index';
 import { Provider } from 'react-redux';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 store.subscribe(() =>
   console.log(store.getState())
