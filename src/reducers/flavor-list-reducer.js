50baf941931ac5e0101569fa57206f7e6428bf3a
Alma <a.q.frankenstein@gmail.com>
2020-12-04 08:45:49 -0800

    pass test to add flavor

--- src/reducers/flavor-list-reducer.js
+++ src/reducers/flavor-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, price, description, quantity, id } = action;
+  switch (action.type) {
+  case 'ADD_FLAVOR':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        description: description,
+        quantity: quantity,
+        id: id
+      }
+    });
+
+  default:
+    return state;
+  }
 };
