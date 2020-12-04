94d5433b6ec85241bf9f638639e3512ae275986f
Alma <a.q.frankenstein@gmail.com>
2020-12-04 11:51:56 -0800

    add action creator and pass test for adding flavor

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -6,4 +6,16 @@ describe('seltzer actions', () => {
       type: 'TOGGLE_FORM'
     });
   });
+
+  it('addFlavor should create ADD_FLAVOR action', () => {
+    expect(actions.addFlavor({name: "Durian", brand: "Bubbeh's Bubbles", price: "2", description: "A delicacy in Singapore", quantity: 124, id:1})).toEqual({
+            type: 'ADD_FLAVOR',
+            name: "Durian", 
+            brand: "Bubbeh's Bubbles", 
+            price: "2", 
+            description: "A delicacy in Singapore",
+            quantity: 124, 
+            id:1
+    });
+  });
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const addFlavor = (flavor) => {
+  const { name, brand, price, description, quantity, id } = flavor;    
+  return {
+    type: 'ADD_FLAVOR',
+    name: name,
+    brand: brand,
+    price: price,
+    description: description,
+    quantity: quantity,
+    id: id
+  }
+}
