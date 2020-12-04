e57af22d3c42ebc11a88bfe517809113fa4f8b9a
Alma <a.q.frankenstein@gmail.com>
2020-12-04 09:24:26 -0800

    add redux code to FlavorControl

--- src/components/FlavorControl.js
+++ src/components/FlavorControl.js
@@ -2,6 +2,8 @@ import React from 'react';
 import NewFlavorForm from './NewFlavorForm';
 import FlavorList from './FlavorList';
 import FlavorDetail from './FlavorDetail';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class FlavorControl extends React.Component {
 
@@ -9,35 +11,60 @@ class FlavorControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false,  
-      masterFlavorList: [{name: "Durian", brand: "Bubbeh's Bubbles", price: "2", description: "A delicacy in Singapore",
-                        quantity: 124}, {name: "PB&J", brand: "Double Bubble", price: "1", description: "Tastes like your favorite sandwhich",
-                        quantity: 124}], 
+      // commented per lesson 12:
+      // masterFlavorList: [{name: "Durian", brand: "Bubbeh's Bubbles", price: "2", description: "A delicacy in Singapore",
+      //                   quantity: 124}, {name: "PB&J", brand: "Double Bubble", price: "1", description: "Tastes like your favorite sandwhich",
+      //                   quantity: 124}], 
       selectedFlavor: null
     };
   }
 
   handleChangingSelectedFlavor = (id) => {
-    const selectedFlavor = this.state.masterFlavorList.filter(flavor => flavor.id === id)[0];
-    this.setState({selectedFlavor: selectedFlavor});
+    // const selectedFlavor = this.state.masterFlavorList.filter(flavor => flavor.id === id)[0];
+    // this.setState({selectedFlavor: selectedFlavor});
+    const selectedFlavor = this.props.masterFlavorList[id];
+    this.setState({selectedFlavor: selectedFlavor})
   }
 
   handleAddingNewFlavorToList = (newFlavor) => {
-    const newMasterFlavorList = this.state.masterFlavorList.concat(newFlavor);
-    this.setState({masterFlavorList: newMasterFlavorList,
-                  formVisibleOnPage: false });
+    // ____________old code, pre lesson 12___________
+    // const newMasterFlavorList = this.state.masterFlavorList.concat(newFlavor);
+    // this.setState({masterFlavorList: newMasterFlavorList,
+    //               formVisibleOnPage: false });
+    // -------------------------------------------------------
+    const { dispatch } = this.props;
+    const { name, brand, price, description, quantity, id } = newFlavor;
+    const action = {
+      type: 'ADD_FLAVOR',
+      name: name,
+      brand: brand,
+      price: price,
+      description: description,
+      quantity: quantity,
+      id: id
+    }
+    dispatch(action);
+    this.setState({formVisibleOnPage: false});
   }
 
   handleSell = (id) => {
-    const newMasterFlavorList = this.state.masterFlavorList;
-    newMasterFlavorList.map((flavor) => {
-      if(flavor.id === id && flavor.quantity > 0) {
-        flavor.quantity -= 1;
-      } else if (flavor.id === id && flavor.quantity === 0) {
-        flavor.quantity = "no more left";
-      }
-      return flavor;
-    });
-    this.setState({masterFlavorList: newMasterFlavorList});
+    //---------------------before adding 'BUY' in flavor-list-reducer-----------
+    // const newMasterFlavorList = this.props.masterFlavorList;
+    // newMasterFlavorList.map((flavor) => {
+    //   if(flavor.id === id && flavor.quantity > 0) {
+    //     flavor.quantity -= 1;
+    //   } else if (flavor.id === id && flavor.quantity === 0) {
+    //     flavor.quantity = "no more left";
+    //   }
+    //   return flavor;
+    // });
+    //-----------------------------------------------------------------
+    const { dispatch } = this.props;
+    const action = {
+      type: 'BUY',
+    }
+    dispatch(action);
+    //this.setState({masterFlavorList: masterFlavorList});
 
   }
 
@@ -66,7 +93,7 @@ class FlavorControl extends React.Component {
       currentlyVisibleState = <NewFlavorForm onNewFlavorCreation={this.handleAddingNewFlavorToList} />;
       buttonText = "return to flavor list";
     } else {
-      currentlyVisibleState = <FlavorList flavorList = {this.state.masterFlavorList} onFlavorSelection= {this.handleChangingSelectedFlavor} onSellFlavor={this.handleSell} />
+      currentlyVisibleState = <FlavorList flavorList = {this.props.masterFlavorList} onFlavorSelection= {this.handleChangingSelectedFlavor} onSellFlavor={this.handleSell} />
       buttonText= "Add flavor";
     }
 
@@ -83,4 +110,15 @@ class FlavorControl extends React.Component {
   }
 }
 
+FlavorControl.propTypes = {
+  masterFlavorList: PropTypes.object
+};
+
+const mapStateToProps = state => {
+  return {
+    masterFlavorList: state
+  }
+}
+
+FlavorControl = connect(mapStateToProps)(FlavorControl);
 export default FlavorControl;
