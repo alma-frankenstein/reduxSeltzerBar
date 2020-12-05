863d9a4b7ade81222a5cb3f72781bf3e1d892e88
Alma <a.q.frankenstein@gmail.com>
2020-12-04 16:07:47 -0800

    revert index.test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,17 +1,15 @@
-import * as actions from '../../actions';
-import * as a from '../../actions/index';
-import * as c from '../../actions/ActionTypes';
+import * as actions from './../../actions';
 
 describe('seltzer actions', () => {
   it('toggleForm should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
-      type: c.TOGGLE_FORM
+      type: 'TOGGLE_FORM'
     });
   });
 
   it('addFlavor should create ADD_FLAVOR action', () => {
     expect(actions.addFlavor({name: "Durian", brand: "Bubbeh's Bubbles", price: "2", description: "A delicacy in Singapore", quantity: 124, id:1})).toEqual({
-            type: c.ADD_FLAVOR,
+            type: 'ADD_FLAVOR',
             name: "Durian", 
             brand: "Bubbeh's Bubbles", 
             price: "2", 
